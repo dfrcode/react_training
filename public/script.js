5e43c9a633a9b235e8b9084e06c6b0110ce9e758
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-10 20:17:13 +0300

    fix script.js/add clear functions and state

--- public/script.js
+++ public/script.js
@@ -16,43 +16,79 @@ const state = {
     ]
 }
 
-const app = createElementDOM('div', 'app'),
-header = createElementDOM('header', 'header'),
-logo = createElementDOM('div', 'logo'),
-clock = createElementDOM('div', 'clock'),
-lots = createElementDOM('div', 'lots');
+render(App({ state }), document.getElementById('root'));
 
-state.lots.forEach(lot => {
+function render(app, root) {
+    
+    root.appendChild(app);
+}
 
+function App({ state }) {
+    const app = createElementDOM('div', 'app');
+
+    app.appendChild(Header(Logo(), Clock({ time: state.time })));
+    app.appendChild(Lots({ lots: state.lots }));
+
+    return app;
+}
+
+function Lots({ lots }) {
+    node = createElementDOM('div', 'lots');
+
+    lots.forEach(lot => {
+        node.appendChild(Lot({ lot }));
+    })
+
+    return node;
+}
+
+function Lot({ lot }) {
+    
     const node = createElementDOM('div', 'lot');
-
+    
     const data = createElementDOM('div', 'data');
-
+    
     const name = createElementDOM('h1', 'name');
     const description = createElementDOM('p', 'description');
     const price = createElementDOM('div', 'price');
-
+    
     name.innerText = lot.name;
     description.innerText = lot.description;
     price.innerText = lot.price;
-
+    
     data.appendChild(name);
     data.appendChild(description);
-
+    
     node.appendChild(data);
     node.appendChild(price);
 
-    lots.appendChild(node);
-});
+    return node;
+}
 
-header.appendChild(logo);
-header.appendChild(clock);
+function Clock({ time }) {
 
-app.appendChild(header);
-app.appendChild(lots);
+    node = createElementDOM('div', 'clock');
+    node.innerText = time;
 
-const root = document.getElementById('root');
-root.appendChild(app);
+    return node;
+}
+
+function Logo() {
+
+    node = createElementDOM('div', 'logo');
+
+    return node;
+}
+
+function Header(logo, clock) {
+
+    node = createElementDOM('header', 'header');
+
+    node.appendChild(logo);
+    node.appendChild(clock);
+
+    return node;
+}
 
 function createElementDOM(el, clN) {
 
